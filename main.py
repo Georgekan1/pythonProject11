49642f200f1b78a3cab3b710c0440a21ba270eec
george <george-kanetounis@hotmail.gr>
2022-11-03 11:26:16 +0100

    conditions

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    a=5
+    b=44
+    c=a+b
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ print(f'C={c}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Hello
+print('Hello to git'
+      '')
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
