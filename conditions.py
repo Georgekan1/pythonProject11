242857feb7680515356625b0981a7a6e58737901
george <george-kanetounis@hotmail.gr>
2022-11-03 11:28:13 +0100

    conditions

---
+++ conditions.py
@@ -0,0 +1,76 @@
+p = True
+q = False
+
+if p:
+    print(' p is True')
+else:
+
+    print('p is False')
+
+if p and q:
+    print('p and q is True')
+else:
+    print('p and q are False')
+if p or q:
+    print('p or q is True')
+else:
+    print('p or q are False')
+
+if p ^ q:
+        print('p xor q is True')
+else:
+        print('p xor q are False')
+
+if p and not q:
+        print('p and not q is True')
+else:
+        print('p and not q are False')
+
+v = None
+
+#if v:
+       # print('v={}'.format(v))
+#else:
+        #print('not v')
+
+#errors for both string and int and other types
+#t = '55'
+#tt = t + v
+#print(tt)
+
+#this will work
+#v = ''
+#t = '55'
+#tt = t + v
+#print(tt)
+
+
+w = 13
+z = 8.8
+
+if w> z:
+    print('w > z')
+else:
+    print('w not > z')
+
+w = 13
+z = 8.8
+
+if w>= z:
+    print('w >= z')
+else:
+    print('w not >= z')
+
+w = 'abce'
+z = 'abc'
+
+if w== z:
+    print('w == z')
+else:
+    print('w not == z')
+
+
+y = 10  if w=='abce'  else 20
+print(f'y={y}')
+print('y={}'.format(y))
+#print('y=' + str(y))
---
+++ email.py
@@ -0,0 +1,42 @@
+email1 = 'pawel.rubach@sgh.waw.pl'
+email2 = 'pawel.rubach#sgh.waw.pl'
+email3 = 'pawel.rubach@.pl'
+email4 = '@sgh.pl'
+
+def check_email(email):
+    if email:
+        return True
+    else:
+        return False
+
+def check_email(email):
+    if email:
+        return True
+    else:
+        return False
+
+if
+    a='@'
+    print(a.find('@'))
+
+y = len(email)
+    for i in range(x):
+        if email[i][0] == "@":
+            return False
+        else email[i][-1] == "@":
+            return False
+        else not email[i].isalnum():
+            return False
+    return True
+
+
+
+for em in [email1, email2, email3, email4]:
+    print('{} : {}'.format(em, check_email(em)))
+    a='pawel.rubach'
+    b='@sgh.waw.pl'
+    print(len(a))
+    print(a.find('p'))
+if len(a)=0 and len(b)=1
+
+    #print(check_email(email1))
